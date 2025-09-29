55e003c4467d0ef82ce26718d31132449b10e166
Dhiya03 <dhiviyalakshmi03@gmail.com>
2025-09-29 10:14:43 +0530

    Update flutter.yml

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -5,14 +5,15 @@ pluginManagement {
         gradlePluginPortal()
     }
 
-    // Flutter's plugin loader (from Flutter SDK)
-    includeBuild("../.flutter")
-}
-
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    // The settings for plugins are applied after the projects are evaluated.
+    // The following closure is executed after the projects are evaluated.
+    plugins {
+        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+        id("com.android.application") version "8.3.2" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    }
 }
 
 include(":app")
+
+apply(from = File(settings.pluginManagement.resolutionStrategy.eachPlugin.find { it.pluginId == "dev.flutter.flutter-plugin-loader" }!!.origin.toString()))
---
+++ lib_backup/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BabyCareApp());
+}
+
+class BabyCareApp extends StatelessWidget {
+  const BabyCareApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Baby Care App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Baby Care App'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to BabyCareApp 👶✨',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
