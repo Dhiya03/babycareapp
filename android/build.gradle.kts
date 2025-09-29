6a6784665d900ce1cd0d3a057cfb283cb20e1732
Dhiya03 <dhiviyalakshmi03@gmail.com>
2025-09-29 07:25:28 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,39 +1,21 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.babycareapp"
-    compileSdk = flutter.compileSdkVersion
-
-    defaultConfig {
-        applicationId = "com.babycareapp"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+// Top-level build.gradle.kts
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-            isMinifyEnabled = false
-            isShrinkResources = false
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.9.25"))
     }
 }
 
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.25")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
-kotlin {
-    jvmToolchain(17)
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
